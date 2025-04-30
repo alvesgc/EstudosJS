fa1613ddea44c19c96ee4aad480556a051f99391
Alisson <alissonalves64@hotmail.com>
2025-04-30 20:56:31 -0300

    feat: rest and array destructuring

--- Desestruturação/arrayDes.js
+++ Desestruturação/arrayDes.js
@@ -1,3 +1,4 @@
 const numeros = [1,2,3,4,5];
-const [primeiroNumero,SegundoNumero] = numeros;
+const [primeiroNumero,SegundoNumero, ...rest] = numeros; // Array Destructuring
 console.log(primeiroNumero,SegundoNumero)
+console.log(rest)

0453d248d9cfdd0e9d828f4fdb17dae67192074b
Alisson <alissonalves64@hotmail.com>
2025-04-30 21:31:03 -0300

    feat: destructuting

--- Desestruturação/arrayDes.js
+++ Desestruturação/arrayDes.js
@@ -1,4 +1,8 @@
-const numeros = [1,2,3,4,5];
-const [primeiroNumero,SegundoNumero, ...rest] = numeros; // Array Destructuring
-console.log(primeiroNumero,SegundoNumero)
-console.log(rest)
+// const numeros = [1,2,3,4,5];
+// const [primeiroNumero,SegundoNumero, ...rest] = numeros; // Array Destructuring
+// console.log(primeiroNumero,SegundoNumero);
+// console.log(rest);
+
+const numeros = [[1,2,3], [4,5,6], [7,8,9]]
+const [list1,list2,list3] = numeros;
+console.log(list3[2])

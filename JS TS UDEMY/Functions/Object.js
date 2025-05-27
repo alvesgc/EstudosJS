d8920292bf9e58b23b00393265bb6377184da0e2
Alisson <alissonalves64@hotmail.com>
2025-05-27 19:58:29 -0300

    feat: Refactor Product constructor to use getter and setter for private stock management

--- JS TS UDEMY/Functions/Object.js
+++ JS TS UDEMY/Functions/Object.js
@@ -1,22 +1,42 @@
 // DefineProperty e DefineProperties
 function Product(nome, preco, estoque) {
+  let estoquePrivado = estoque;
   Object.defineProperty(this, 'estoque', {
     enumerable: true, // mostra a propriedade
-    value: estoque, // valor
-    writable: true, // pode ser alterado
-    configurable: false // não pode ser deletado
+    configurable: false, // não pode ser deletado
+     get: function() {
+        return estoquePrivado;
+      },
+      set: function(valor) {
+        if (typeof valor !== 'number') {
+          throw new TypeError('Valor inválido');
+        }
+        estoquePrivado = valor;
+      }
   })
 
-  Object.defineProperties(this, {
-    nome: {
-      enumerable: true, // mostra a propriedade
-      value: estoque, // valor
-      writable: true, // pode ser alterado
-      configurable: false // não pode ser deletado
-    }
-  })
+  // Object.defineProperties(this, {
+  //   nome: {
+  //     enumerable: true, // mostra a propriedade
+  //     configurable: false, // não pode ser deletado
+  //   }
+  // })
 }
 
-const p1 = new Product('Caneta', 1.99, 1000);
-console.log(p1.estoque); // 1000
-console.log(p1);
+// Exemplo de uso Getter e Setter em Factory Function
+function criaProduto(nome)  {
+  return {
+    get nome() {
+      return nome;
+    },
+    set nome(valor) {
+      nome = valor;
+    }
+  }
+}
+
+const p2 = criaProduto('Caneta');
+console.log(p2.nome); 
+// const p1 = new Product('Caneta', 1.99, 1000);
+// p1.estoque = 'teste';
+// console.log(p1.estoque); // 1000
